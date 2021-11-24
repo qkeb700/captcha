86557809e7d4d5c535001a9a6a843b0a4f974131
Hoyeon Jang <46728564+qkeb700@users.noreply.github.com>
2021-11-25 00:30:27 +0900

    started working on catpcha
    created the project

---
+++ src/main/java/captcha/CaptcCha.java
@@ -0,0 +1,14 @@
+package captcha;
+import java.io.IOException;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+public class CaptcCha {
+	private static final long serialVersionUID = 1L;
+	private static int width = 150;
+	private static int height = 50;
+	
+	public void getCaptCha(HttpServletRequest req, HttpServletResponse res) throws IOException{
+		
+	}
+}
